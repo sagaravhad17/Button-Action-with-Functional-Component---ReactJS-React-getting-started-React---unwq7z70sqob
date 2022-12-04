e75d72110e341eea940bd66f91c552bab150d708
Sagar Dharmanath Avhad <avhadsagar00@gmail.com>
2022-12-04 17:15:48 +0530

    ButtonAction

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para,setPara] = useState("")
+
+  const handlePara = ()=>{
+    setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id="para">{para}</p>
+      <button id="click" onClick={handlePara}>Button</button>
     </div>
   );
 }
